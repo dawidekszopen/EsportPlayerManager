17304c4a284e7712cc790d09c0035e999b930c94
dawidekszopen <62569747+dawidekszopen@users.noreply.github.com>
2025-05-01 16:46:52 +0200

    idk

--- EsportPlayerManager/Services/PlayerService.cs
+++ EsportPlayerManager/Services/PlayerService.cs
@@ -1,6 +1,14 @@
+using EsportPlayerManager.Repositorys;
+
 namespace EsportPlayerManager.Services;
 
 public class PlayerService
 {
+    private readonly PlayerRepository _playerRepository;
+
+    public PlayerService(PlayerRepository playerRepository)
+    {
+        _playerRepository = playerRepository;
+    }
     
 }
